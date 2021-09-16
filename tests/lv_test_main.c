954baf3a9b01c3a870d833c39d90532616f4b473
lxowalle <lxowalle@outlook.com>
2021-09-16 17:26:43 +0800

    [clear]

--- tests/lv_test_main.c
+++ tests/lv_test_main.c
@@ -124,22 +124,22 @@ static void dummy_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_
     lv_disp_flush_ready(disp_drv);
 }
 
-uint32_t custom_tick_get(void)
-{
-    static uint64_t start_ms = 0;
-    if(start_ms == 0) {
-        struct timeval tv_start;
-        gettimeofday(&tv_start, NULL);
-        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
-    }
+// uint32_t custom_tick_get(void)
+// {
+//     static uint64_t start_ms = 0;
+//     if(start_ms == 0) {
+//         struct timeval tv_start;
+//         gettimeofday(&tv_start, NULL);
+//         start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
+//     }
 
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    uint64_t now_ms;
-    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
+//     struct timeval tv_now;
+//     gettimeofday(&tv_now, NULL);
+//     uint64_t now_ms;
+//     now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
 
-    uint32_t time_ms = now_ms - start_ms;
-    return time_ms;
-}
+//     uint32_t time_ms = now_ms - start_ms;
+//     return time_ms;
+// }
 
-#endif
+// #endif
